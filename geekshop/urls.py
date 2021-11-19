a13a360703a5b286881f237c7fe83bb5055190c5
k.barylnikov <ipt_git_bot@ipt.ru>
2021-11-20 02:42:12 +0300

    add models, migrations, media, url dispatcher

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import products.views as products
+from geekshop import settings
 
 urlpatterns = [
     path('', products.main, name='main'),
-    path('products/', products.products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -3,8 +3,21 @@ from django.db import models
 # Create your models here.
 
 
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    img = models.CharField(max_length=100)
-    cost = models.CharField(max_length=10)
-    desc = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='products_uploads/', blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    desc = models.TextField(max_length=100)
+    quality = models.PositiveIntegerField()
+    category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
