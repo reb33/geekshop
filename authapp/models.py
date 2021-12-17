6c9616b0d54e4ac3075f188eddcae1963b8ffec8
k.barylnikov <ipt_git_bot@ipt.ru>
2021-12-17 18:33:00 +0300

    lesson2_1: add verification user through email, add context_processor basket

--- authapp/models.py
+++ authapp/models.py
@@ -1,9 +1,19 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from datetime import timedelta
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 # Create your models here.
+from django.utils.timezone import now
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст', default=18)
+
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(auto_now=True, blank=True)
+
+    def is_activation_key_expires(self):
+        if now() <= self.activation_key_expires + timedelta(hours=48):
+            return False
+        return True
